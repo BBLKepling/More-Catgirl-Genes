619af50b216427cfde8914937f4aa08514275d2a
BBLKepling <90485361+BBLKepling@users.noreply.github.com>
2025-06-24 08:34:47 -0700

    1.6 update

---
+++ 1.6/DBH/Source/More Catgirl Genes/HarmonyInit.cs
@@ -0,0 +1,24 @@
+﻿using DubsBadHygiene;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace More_Catgirl_Genes
+{
+    [StaticConstructorOnStartup]
+    public static class HarmonyInit
+    {
+        static HarmonyInit()
+        {
+            Harmony harmonyInstance = new Harmony("BBLKepling.MoreCatgirlGenes");
+            harmonyInstance.Patch(typeof(JobGiver_UseToilet).GetMethod("TryGiveJob"), prefix: new HarmonyMethod(typeof(JobGiver_UseToilet_Patch).GetMethod("Prefix")));
+            if (ModLister.HasActiveModWithName("Dubs Bad Hygiene")) harmonyInstance.Patch(typeof(JobGiver_HaveWash).GetMethod("TryGiveJob"), prefix: new HarmonyMethod(typeof(JobGiver_HaveWash_Patch).GetMethod("PrefixReg")));
+            if (ModLister.HasActiveModWithName("Dubs Bad Hygiene Lite")) harmonyInstance.Patch(typeof(JobGiver_HaveWash).GetMethod("TryGiveJob"), prefix: new HarmonyMethod(typeof(JobGiver_HaveWash_Patch).GetMethod("PrefixLite")));
+            if (ModLister.HasActiveModWithName("Carry A Barf Bag"))
+            {
+                harmonyInstance.Patch(AccessTools.Method(typeof(JobDriver_Vomit), "MakeNewToils"), prefix: new HarmonyMethod(typeof(JobDriver_Vomit_Patch).GetMethod("PrefixBarfBag")), postfix: new HarmonyMethod(typeof(JobDriver_Vomit_Patch).GetMethod("PostfixBarfBag")));
+            }
+            else harmonyInstance.Patch(AccessTools.Method(typeof(JobDriver_Vomit), "MakeNewToils"), postfix: new HarmonyMethod(typeof(JobDriver_Vomit_Patch).GetMethod("PostfixVanilla")));
+        }
+    }
+}
---
+++ 1.6/DBH/Source/More Catgirl Genes/JobDriver_LickSelf.cs
@@ -0,0 +1,101 @@
+﻿using DubsBadHygiene;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace More_Catgirl_Genes
+{
+    public class JobDriver_LickSelf : JobDriver
+    {
+        private bool bionicTongue;
+        private bool catLax;
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            return true;
+        }
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            yield return Toils_GoToLickSpot.GoToLickSpot(pawn);
+            Toil toil = new Toil
+            {
+                debugName = "MakeNewToils",
+                defaultDuration = 1000,
+                defaultCompleteMode = ToilCompleteMode.Delay
+            };
+            toil.WithEffect(DubDef.WashingEffect, TargetIndex.A);
+            if (ModLister.HasActiveModWithName("Dubs Bad Hygiene"))
+            {
+                toil.AddEndCondition(() => (pawn.needs.TryGetNeed<Need_Hygiene>().CurLevel < (bionicTongue ? 0.75f : 0.65f) && pawn.needs?.TryGetNeed<Need_Thirst>().CurLevel >= 0.30f) ? JobCondition.Ongoing : JobCondition.Succeeded);
+                toil.tickAction = delegate
+                {
+                    pawn.needs.TryGetNeed<Need_Hygiene>().clean(bionicTongue ? 0.002f : 0.001f);
+                    if (ticksLeftThisToil % 3 != 0) return;
+                    pawn.needs.TryGetNeed<Need_Thirst>().Drink(-0.001f);
+                    if (ticksLeftThisToil % 60 != 0) return;
+                    if (Rand.Range(1, 6) == 1)
+                    {
+                        FilthMaker.TryMakeFilth(pawn.Position, pawn.Map, ThingDefOf.Filth_AnimalFilth, pawn.LabelIndefinite(), 1, FilthSourceFlags.Pawn);
+                        return;
+                    }
+                    if (!(pawn.health?.hediffSet is HediffSet hediffset)) return;
+                    if (hediffset.TryGetHediff(InternalDefOf.BBLK_Hairball, out Hediff hediff))
+                    {
+                        hediff.Severity += catLax ? (Rand.Range(1, 4) * -.01f) : (Rand.Range(1, 4) * .01f);
+                        if (catLax && hediff.Severity <= .01f) pawn.health.RemoveHediff(hediff);
+                        return;
+                    }
+                    if (catLax) return;
+                    BodyPartRecord stomach = hediffset.GetBodyPartRecord(InternalDefOf.Stomach);
+                    if (!hediffset.PartOrAnyAncestorHasDirectlyAddedParts(stomach) && hediffset.PartIsMissing(stomach)) return;
+                    pawn.health.AddHediff(InternalDefOf.BBLK_Hairball, stomach);
+                };
+            }
+            else
+            {
+                toil.AddEndCondition(() => (pawn.needs.TryGetNeed<Need_Hygiene>().CurLevel < (bionicTongue ? 0.75f : 0.65f)) ? JobCondition.Ongoing : JobCondition.Succeeded);
+                toil.tickAction = delegate
+                {
+                    pawn.needs.TryGetNeed<Need_Hygiene>().clean(bionicTongue ? 0.002f : 0.001f);
+                    if (ticksLeftThisToil % 60 != 0) return;
+                    if (Rand.Range(1, 6) == 1)
+                    {
+                        FilthMaker.TryMakeFilth(pawn.Position, pawn.Map, ThingDefOf.Filth_AnimalFilth, pawn.LabelIndefinite(), 1, FilthSourceFlags.Pawn);
+                        return;
+                    }
+                    if (!(pawn.health?.hediffSet is HediffSet hediffset)) return;
+                    if (hediffset.TryGetHediff(InternalDefOf.BBLK_Hairball, out Hediff hediff))
+                    {
+                        hediff.Severity += catLax ? (Rand.Range(1, 4) * -.01f) : (Rand.Range(1, 4) * .01f);
+                        if (catLax && hediff.Severity <= .01f) pawn.health.RemoveHediff(hediff);
+                        return;
+                    }
+                    if (catLax) return;
+                    BodyPartRecord stomach = hediffset.GetBodyPartRecord(InternalDefOf.Stomach);
+                    if (!hediffset.PartOrAnyAncestorHasDirectlyAddedParts(stomach) && hediffset.PartIsMissing(stomach)) return;
+                    pawn.health.AddHediff(InternalDefOf.BBLK_Hairball, stomach);
+                };
+            }
+            toil.AddFinishAction(delegate
+            {
+                pawn.CleanContamination();
+                SanitationUtil.ContaminationFromCellForPawn(pawn, pawn.CurJob.GetTarget(TargetIndex.A).Cell);
+            });
+            yield return toil;
+        }
+        public override void Notify_Starting()
+        {
+            base.Notify_Starting();
+            job.count = 1;
+            job.targetA = pawn;
+            bionicTongue = pawn.health.hediffSet.HasHediff(InternalDefOf.BionicTongue);
+            catLax = ModLister.HasActiveModWithName("Kepling's Hairball Mod") && pawn.health.hediffSet.HasHediff(InternalDefOf.BBLK_CatLax);
+        }
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref bionicTongue, "bionicTongue", defaultValue: false);
+            Scribe_Values.Look(ref catLax, "catLax", defaultValue: false);
+        }
+    }
+}
